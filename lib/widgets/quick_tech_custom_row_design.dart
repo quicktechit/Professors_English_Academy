01085457d5d601576287bfd60833564e04f796ed
nrkhan-223 <redoykhan8141@gmail.com>
2025-05-20 14:06:46 +0600

    minor bug fix & Play Store upload

--- lib/widgets/quick_tech_custom_row_design.dart
+++ lib/widgets/quick_tech_custom_row_design.dart
@@ -3,7 +3,7 @@ import 'package:professors_english_academy/consts/consts.dart';
 Widget customRow(BuildContext context, String title, String subtitle,onTap) {
   return Row(
     children: [
-      title.tr.text.xl2.semiBold.make(),
+      title.tr.text.xl2.semiBold.make().w(context.screenWidth/1.5),
       Spacer(),
       subtitle.text.semiBold.xl.color(mainColor).make().onTap(onTap)
     ],
